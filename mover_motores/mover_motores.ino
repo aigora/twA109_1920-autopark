14d1c2a12bda9c0ba0abc423b706debdd328f9a1
adrismap <61022558+adrismap@users.noreply.github.com>
2020-05-12 16:14:07 +0200

    Update mover_motores.ino

--- mover_motores/mover_motores.ino
+++ mover_motores/mover_motores.ino
@@ -1,14 +1,25 @@
 #include <AFMotor.h>
 AF_DCMotor Motor1(1);
+AF_DCMotor Motor2(2);
+AF_DCMotor Motor3(3);
+AF_DCMotor Motor4(4);
 
 void setup()
-   {        Motor1.run(RELEASE);     }
+   {        Motor1.run(RELEASE);
+            Motor2.run(RELEASE);
+            Motor3.run(RELEASE);
+            Motor14run(RELEASE);
+   }
 
 void loop()
    {   Motor1.run(FORWARD); 
-       delay (2000);
+       Motor2.run(FORWARD);
+       Motor3.run(FORWARD); 
+       Motor4.run(FORWARD);
 
-       Motor1.setSpeed(180);
-       Motor1.run(BACKWARD);
-       delay (2000);
+       Motor1.setSpeed(200);
+       Motor2.setSpeed(200);
+       Motor3.setSpeed(200);
+       Motor4.setSpeed(200);
+       
    }
